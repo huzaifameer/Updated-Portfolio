68bc0c429e555cfbec5bc458ceef22fd95b8975e
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-24 19:19:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Content from './components/Content/Content';
 
 function App() {
   return (
     <div className="App">
-      
+      <Content></Content>
     </div>
   );
 }
---
+++ src/components/Content/Content.js
@@ -0,0 +1,11 @@
+
+import "./Content-styles.css"
+function Content(){
+    return(
+        <div>
+            <h1 className='.content-outer'></h1>
+            Content Section
+        </div>
+    );
+}
+export default Content;
